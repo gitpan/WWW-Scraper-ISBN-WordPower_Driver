40434e3c1b2d51bfbdbf5303905fa0b2ffdcf461
Barbie <barbie@missbarbell.co.uk>
2010-10-06 11:43:10 +0000

    Import of BARBIE/WWW-Scraper-ISBN-WordPower_Driver-0.03 from CPAN.
    gitpan-cpan-distribution: WWW-Scraper-ISBN-WordPower_Driver
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         BARBIE/WWW-Scraper-ISBN-WordPower_Driver-0.03.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/WWW/Scraper/ISBN/WordPower_Driver.pm
+++ lib/WWW/Scraper/ISBN/WordPower_Driver.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION @ISA);
-$VERSION = '0.02';
+$VERSION = '0.03';
 
 #--------------------------------------------------------------------------
 
@@ -51,8 +51,8 @@ my ($URL1,$URL2,$URL3) = ('http://www.word-power.co.uk','/books/[^>]+-I','/');
 
 =item C<search()>
 
-Creates a query string, then passes the appropriate form fields to the Word 
-Power server.
+Creates a query string, then passes the appropriate form fields to the
+Word Power server.
 
 The returned page should be the correct catalog page for that ISBN. If not the
 function returns zero and allows the next driver in the chain to have a go. If
